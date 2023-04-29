cb9ac52526d6e4dceeae9d603f5a8414d09909dc
Ayjent <121435895+Ayjent@users.noreply.github.com>
2023-04-29 07:07:54 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BRS Translator'
+copyright = '2023, Jay Besch'
+author = 'Jay Besch'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.93'
 
 # -- General configuration
 
